61d02b4ccb6f9cb76781c315bd7d5a03bcb6bffd
dazo66 <99306813+dazo6@users.noreply.github.com>
2025-10-30 19:55:39 +0800

    Change webView URL to a different site

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -143,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // 这里填你需要打包的 H5 页面链接
-        webView.loadUrl("https://baidu.com");
+        webView.loadUrl("https://zh.xhamster.com/");
 
         //显示一些小图片（头像）
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -159,7 +159,7 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
 
-        startForegroundService();
+        // startForegroundService();
 
         webView.loadUrl("javascript:(function() { " +
             "var audio = document.getElementById('alarmSound');" +
